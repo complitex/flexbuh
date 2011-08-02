12879ddab9f326cbfa9ed59ae575eeaafbf7cc58
heaven.iaa <heaven.iaa@931d702a-5a61-2881-6589-5da9c06b4b28>
2011-08-02 08:19:51 +0000

    change create database script

--- flexbuh-db/flexbuh-create.sql
+++ flexbuh-db/flexbuh-create.sql
@@ -1,17 +1,61 @@
--- ------------------------------
--- User Organization //todo backward compatibility
--- ------------------------------
-DROP TABLE IF EXISTS `user_organization`;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 
-CREATE TABLE `user_organization` (
-    `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор организации пользователей',
-    `user_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор пользователя',
-    `organization_object_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор организации',
-    `main` TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'Является ли организация основной',
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `key_unique` (`user_id`, `organization_object_id`),
-    CONSTRAINT `fk_user_organization__user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Организация пользователей';
+-- ------------------------------
+-- User
+-- ------------------------------
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE  `user` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор пользователя',
+  `login` VARCHAR(45) NOT NULL COMMENT 'Имя пользователя',
+  `password` VARCHAR(45) NOT NULL COMMENT 'MD5 хэш пароля',
+  `user_info_object_id` BIGINT(20) COMMENT 'Идентификатор объекта информация о пользователе',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_key_login` (`login`),
+  KEY `key_user_info_object_id` (`user_info_object_id`),
+  CONSTRAINT `fk_user__user_info` FOREIGN KEY (`user_info_object_id`) REFERENCES `user_info` (`object_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Пользователь';
+
+-- ------------------------------
+-- Usergroup
+-- ------------------------------
+DROP TABLE IF EXISTS `usergroup`;
+
+CREATE TABLE  `usergroup` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор группы пользователей',
+  `login` VARCHAR(45) NOT NULL COMMENT 'Имя пользователя',
+  `group_name` VARCHAR(45) NOT NULL COMMENT 'Название группы',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_login__group_name` (`login`, `group_name`),
+  CONSTRAINT `fk_usergroup__user` FOREIGN KEY (`login`) REFERENCES `user` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Группа пользователей';
+
+-- ------------------------------
+-- Config
+-- ------------------------------
+
+DROP TABLE IF EXISTS `config`;
+
+CREATE TABLE `config` (
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор настройки',
+    `name` VARCHAR(64) NOT NULL COMMENT 'Имя',
+    `value` VARCHAR(255) NOT NULL COMMENT 'Значение',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Настройки';
+
+-- ------------------------------
+-- Update
+-- ------------------------------
+
+DROP TABLE IF EXISTS `update`;
+
+CREATE TABLE `update` (
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор обновления',
+    `version` VARCHAR(64) NOT NULL COMMENT 'Версия',
+    `date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата обновления',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Обновление базы данных';
+
 
 -- --------------------------
 -- XSL
@@ -57,3 +101,6 @@ CREATE TABLE `template_xsd` (
   UNIQUE INDEX `key_name`(`name`)
 )
 ENGINE = InnoDB DEFAULT CHARSET=utf8;
+
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+
