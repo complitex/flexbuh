e99ceb2965196cd250f7b6de413b4fc138ca681d
pavel.sknar@gmail.com <pavel.sknar@gmail.com@931d702a-5a61-2881-6589-5da9c06b4b28>
2011-08-04 08:01:43 +0000

    FB-1 add create pool script

---
+++ flexbuh-db/create-pool.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+GLASSFISH_ASADMIN_COMMAND=/home/pavel/glassfish3/bin/asadmin
+GLASSFISH_POOL=flexbuhPool
+DB_USER=flexbuh
+DB_PASSWORD=flexbuh
+DB_URL=jdbc\\:mysql\\://localhost\\:3306/flexbuh
+
+echo "---------------------------------------------------"
+echo "Local database and Realm"
+echo "---------------------------------------------------"
+echo "Register the JDBC connection pool"
+$GLASSFISH_ASADMIN_COMMAND create-jdbc-connection-pool --driverclassname com.mysql.jdbc.Driver --restype java.sql.Driver --property "URL=$DB_URL:User=$DB_USER:Password=$DB_PASSWORD" $GLASSFISH_POOL
+
+echo "Create a JDBC resource with the specified JNDI name"
+$GLASSFISH_ASADMIN_COMMAND create-jdbc-resource --connectionpoolid $GLASSFISH_POOL jdbc/flexbuhResource
+
+echo "Add the named authentication realm"
+$GLASSFISH_ASADMIN_COMMAND create-auth-realm --classname com.sun.enterprise.security.auth.realm.jdbc.JDBCRealm --property jaas-context=jdbcRealm:datasource-jndi=jdbc/flexbuhResource:user-table=user:user-name-column=login:password-column=password:group-table=usergroup:group-name-column=group_name:charset=UTF-8:digest-algorithm=MD5 flexbuhRealm
+
