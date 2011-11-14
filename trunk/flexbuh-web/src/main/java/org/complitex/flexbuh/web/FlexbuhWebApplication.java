7ad735ef304d4e18699f4d4fc2aed0f640bf9cb0
heaven.iaa <heaven.iaa@931d702a-5a61-2881-6589-5da9c06b4b28>
2011-11-14 07:28:39 +0000

    add login

--- trunk/flexbuh-web/src/main/java/org/complitex/flexbuh/web/FlexbuhWebApplication.java
+++ trunk/flexbuh-web/src/main/java/org/complitex/flexbuh/web/FlexbuhWebApplication.java
@@ -1,10 +1,12 @@
 package org.complitex.flexbuh.web;
 
 import org.apache.wicket.Page;
+import org.complitex.flexbuh.template.TemplateWebApplication;
+import org.complitex.flexbuh.template.pages.login.Login;
+
 //import org.complitex.flexbuh.document.test.MarkupTestPage;
 //import org.complitex.flexbuh.document.test.TransformationTestPage;
 //import org.complitex.flexbuh.document.test.ValidationTestPage;
-import org.complitex.flexbuh.template.TemplateWebApplication;
 
 /**
  * @author Anatoly A. Ivanov java@inheaven.ru
@@ -16,17 +18,14 @@ public class FlexbuhWebApplication extends TemplateWebApplication{
     protected void init() {
         super.init();
 
-//        mountBookmarkablePage("test2", TransformationTestPage.class);
-//        mountBookmarkablePage("test3", MarkupTestPage.class);
-//        mountBookmarkablePage("test4", ValidationTestPage.class);
+        mountPage("login", Login.class);
 
-        getDebugSettings().setComponentUseCheck(true);
-        getDebugSettings().setLinePreciseReportingOnAddComponentEnabled(true);
-        getDebugSettings().setLinePreciseReportingOnNewComponentEnabled(true);
-        getDebugSettings().setOutputComponentPath(true);
-        getDebugSettings().setOutputMarkupContainerClassName(true);
-        getDebugSettings().setDevelopmentUtilitiesEnabled(true);
-
+//        getDebugSettings().setComponentUseCheck(true);
+//        getDebugSettings().setLinePreciseReportingOnAddComponentEnabled(true);
+//        getDebugSettings().setLinePreciseReportingOnNewComponentEnabled(true);
+//        getDebugSettings().setOutputComponentPath(true);
+//        getDebugSettings().setOutputMarkupContainerClassName(true);
+//        getDebugSettings().setDevelopmentUtilitiesEnabled(true);
     }
 
     @Override
