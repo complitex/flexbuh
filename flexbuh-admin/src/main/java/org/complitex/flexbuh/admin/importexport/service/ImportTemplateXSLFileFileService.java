02009ac610bc9b05b15b101084cd415fff0f0b4f
heaven.iaa <heaven.iaa@931d702a-5a61-2881-6589-5da9c06b4b28>
2011-11-22 11:47:01 +0000

    change wicket version to 1.5.3, fix template file import

--- flexbuh-admin/src/main/java/org/complitex/flexbuh/admin/importexport/service/ImportTemplateXSLFileFileService.java
+++ flexbuh-admin/src/main/java/org/complitex/flexbuh/admin/importexport/service/ImportTemplateXSLFileFileService.java
@@ -1,8 +1,8 @@
 package org.complitex.flexbuh.admin.importexport.service;
 
 import javax.ejb.Stateless;
-import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author Pavel Sknar
@@ -12,8 +12,8 @@ import java.io.IOException;
 public class ImportTemplateXSLFileFileService extends ImportTemplateFileService {
 
 	@Override
-	protected String getData(File file) throws IOException {
-		return changeWhitespaceUTF8(super.getData(file));
+	protected String getData(InputStream inputStream) throws IOException {
+		return changeWhitespaceUTF8(super.getData(inputStream));
 	}
 
 	private String changeWhitespaceUTF8(String s){
