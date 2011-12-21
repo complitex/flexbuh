a78f07aebd41b5edaeb0cb2fa2e7946cce196b23
pavel.sknar@gmail.com <pavel.sknar@gmail.com@931d702a-5a61-2881-6589-5da9c06b4b28>
2011-12-21 09:10:35 +0000

    Added user list

---
+++ flexbuh-admin/src/main/java/org/complitex/flexbuh/admin/user/web/UserList.java
@@ -0,0 +1,194 @@
+package org.complitex.flexbuh.admin.user.web;
+
+import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.OrderByBorder;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.DropDownChoice;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.IChoiceRenderer;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.model.util.ListModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.complitex.flexbuh.common.entity.user.User;
+import org.complitex.flexbuh.common.security.SecurityRole;
+import org.complitex.flexbuh.common.service.user.UserBean;
+import org.complitex.flexbuh.common.service.user.UserFilter;
+import org.complitex.flexbuh.common.template.TemplatePage;
+import org.complitex.flexbuh.common.template.toolbar.AddUserButton;
+import org.complitex.flexbuh.common.template.toolbar.ToolbarButton;
+import org.complitex.flexbuh.common.web.component.BookmarkablePageLinkPanel;
+import org.complitex.flexbuh.common.web.component.datatable.DataProvider;
+import org.complitex.flexbuh.common.web.component.paging.PagingNavigator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Pavel Sknar
+ *         Date: 19.12.11 12:07
+ */
+@AuthorizeInstantiation(SecurityRole.ADMIN_MODULE_EDIT)
+public class UserList extends TemplatePage {
+
+    private final static Logger log = LoggerFactory.getLogger(UserList.class);
+
+    @EJB
+    private UserBean userBean;
+
+    public UserList() {
+
+        add(new Label("title", new ResourceModel("title")));
+        add(new FeedbackPanel("messages"));
+
+        log.debug("get user: {}", userBean.getUser(1L));
+
+        //Фильтр
+        UserFilter filterObject = new UserFilter();
+        final IModel<UserFilter> filterModel = new Model<UserFilter>(filterObject);
+
+        final Form filterForm = new Form("filter_form");
+        filterForm.setOutputMarkupId(true);
+        add(filterForm);
+
+        Link filterReset = new Link("reset") {
+
+            @Override
+            public void onClick() {
+                filterForm.clearInput();
+
+                filterModel.setObject(new UserFilter());
+            }
+        };
+        filterForm.add(filterReset);
+
+        filterForm.add(new TextField<String>("login", new PropertyModel<String>(filterModel, "login")));
+        filterForm.add(new TextField<String>("last_name", new PropertyModel<String>(filterModel, "lastName")));
+        filterForm.add(new TextField<String>("first_name", new PropertyModel<String>(filterModel, "firstName")));
+        filterForm.add(new TextField<String>("middle_name", new PropertyModel<String>(filterModel, "middleName")));
+        filterForm.add(new TextField<String>("address", new PropertyModel<String>(filterModel, "address")));
+
+        filterForm.add(new DropDownChoice<String>("roles",
+                new PropertyModel<String>(filterModel, "role"),
+                new ListModel<String>(userBean.getAllRoles()),
+                new IChoiceRenderer<String>() {
+
+                    @Override
+                    public Object getDisplayValue(String object) {
+                        return getStringOrKey(object);
+                    }
+
+                    @Override
+                    public String getIdValue(String object, int index) {
+                        return object;
+                    }
+                }));
+
+        //Модель
+        final DataProvider<User> dataProvider = new DataProvider<User>() {
+
+            @Override
+            protected Iterable<? extends User> getData(int first, int count) {
+                UserFilter filter = filterModel.getObject();
+                filter.setFirst(first);
+                filter.setCount(count);
+                filter.setSortProperty(getSort().getProperty());
+
+                filter.setAscending(getSort().isAscending());
+                return userBean.getUsers(filter);
+            }
+
+            @Override
+            protected int getSize() {
+                return userBean.getUsersCount(filterModel.getObject());
+            }
+        };
+        dataProvider.setSort("login", SortOrder.ASCENDING);
+
+        //Таблица
+        DataView<User> dataView = new DataView<User>("users", dataProvider, 10) {
+
+            @Override
+            protected void populateItem(Item<User> item) {
+                User user = item.getModelObject();
+
+                item.add(new Label("login", user.getLogin()));
+
+                item.add(new Label("role", getDisplayRoleNames(user)));
+
+                item.add(new Label("last_name", user.getLastName()));
+
+                item.add(new Label("first_name", user.getLastName()));
+
+                item.add(new Label("middle_name", user.getLastName()));
+
+                item.add(new Label("address", user.getLastName()));
+
+                PageParameters pageParameters = new PageParameters();
+
+                pageParameters.set("user_id", user.getId());
+                item.add(new BookmarkablePageLinkPanel<User>("action_edit", getString("action_edit"),
+                        UserEdit.class, pageParameters));
+
+                pageParameters.set("action", "copy");
+                item.add(new BookmarkablePageLinkPanel<User>("action_copy", getString("action_copy"),
+                        UserEdit.class, pageParameters));
+            }
+        };
+        filterForm.add(dataView);
+
+        //Названия колонок и сортировка
+        filterForm.add(new OrderByBorder("header.login", "login", dataProvider));
+        filterForm.add(new OrderByBorder("header.last_name", "lastName", dataProvider));
+        filterForm.add(new OrderByBorder("header.first_name", "firstName", dataProvider));
+        filterForm.add(new OrderByBorder("header.middle_name", "middleName", dataProvider));
+        filterForm.add(new OrderByBorder("header.address", "address", dataProvider));
+
+        //Постраничная навигация
+        filterForm.add(new PagingNavigator("paging", dataView, UserList.class.getName(), filterForm));
+    }
+
+    /**
+     * Генерирует строку списка групп пользователей для отображения
+     * @param user Пользователь
+     * @return Список групп
+     */
+    private String getDisplayRoleNames(User user) {
+        if (user.getRoles() == null || user.getRoles().isEmpty()) {
+            return getString("blocked");
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Iterator<String> it = user.getRoles().iterator();;) {
+            sb.append(getString(it.next()));
+            if (!it.hasNext()) {
+                return sb.toString();
+            }
+            sb.append(", ");
+        }
+    }
+
+    @Override
+    protected List<ToolbarButton> getToolbarButtons(String id) {
+        return Arrays.asList((ToolbarButton) new AddUserButton(id) {
+
+            @Override
+            protected void onClick() {
+                setResponsePage(UserEdit.class);
+            }
+        });
+    }
+}
--- flexbuh-common/src/main/java/org/complitex/flexbuh/common/service/user/UserBean.java
+++ flexbuh-common/src/main/java/org/complitex/flexbuh/common/service/user/UserBean.java
@@ -1,27 +1,116 @@
 package org.complitex.flexbuh.common.service.user;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.complitex.flexbuh.common.entity.user.User;
+import org.complitex.flexbuh.common.mybatis.Transactional;
 import org.complitex.flexbuh.common.service.AbstractBean;
 
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.complitex.flexbuh.common.security.SecurityRole.*;
 
 /**
  * @author Pavel Sknar
  *         Date: 01.09.11 14:56
  */
 @Stateless
-public class UserBean extends AbstractBean{
+public class UserBean extends AbstractBean {
 	public static final String NS = UserBean.class.getName();
 
-	@EJB
-	private SessionBean sessionBean;
+    private static final List<String> ALL_ROLES = Lists.newArrayList(
+            AUTHORIZED,
+            ADMIN_MODULE_EDIT,
+            LOG_VIEW);
+
+    private static final Map<String, List<String>> GROUPS = Maps.newHashMap();
+
+    static {
+        GROUPS.put("ADMINISTRATORS", Lists.<String>newArrayList(AUTHORIZED, ADMIN_MODULE_EDIT, LOG_VIEW));
+        GROUPS.put("EMPLOYEES", Lists.<String>newArrayList(AUTHORIZED));
+
+        ALL_ROLES.addAll(GROUPS.keySet());
+    }
+
+    @Transactional
+    public void create(User user) {
+        user.setPassword(DigestUtils.md5Hex(user.getLogin())); //md5 password
+
+        sqlSession().insert(NS + ".insertUser", user);
+
+        //сохранение привилегий
+        Map<String, String> newRole = Maps.newHashMap();
+        newRole.put("login", user.getLogin());
+        for(String role : user.getRoles()){
+            newRole.put("role", role);
+            sqlSession().insert(NS + ".insertUserGroup", newRole);
+        }
+    }
+
+    @Transactional
+    public void update(User user) {
+        User dbUser = (User) sqlSession().selectOne(NS + ".selectUser", user.getId());
+
+        //удаление групп привилегий
+        Collections.sort(user.getRoles());
+        Map<String, String> role = Maps.newHashMap();
+        role.put("login", user.getLogin());
+        int i = 0, j = -1;
+        for (int k = 0 ; k < dbUser.getRoles().size(); k++) {
+
+            String dbUserRole = dbUser.getRoles().get(k);
+
+            while (i < user.getRoles().size() || j == 0) {
+                j = dbUserRole.compareTo(user.getRoles().get(i));
+                role.put("role", dbUserRole);
+                if (j < 0) {
+                    sqlSession().delete(NS + ".deleteUserRole", role);
+                    if (k < dbUser.getRoles().size()) {
+                        break;
+                    }
+                } else if (j > 0) {
+                    sqlSession().delete(NS + ".insertUserRole", role);
+                }
+                i++;
+            }
+
+            j = -1;
+        }
+    }
 
 	public User getUser(long id) {
-		return (User) sqlSession().selectOne(NS + ".selectUser", id);
+		return (User) sqlSession().selectOne(NS + ".selectUserById", id);
 	}
 
-    public User getUserBySessionId(Long sessionId){
+    public User getUserBySessionId(Long sessionId) {
         return (User)sqlSession().selectOne(NS + ".selectUserBySessionId", sessionId);
+    }
+
+    @SuppressWarnings({"unchecked"})
+    public List<User> getUsers(UserFilter filter) {
+        appendRoleGroup(filter);
+        return sqlSession().selectList(NS + ".selectUsers", filter);
+    }
+
+    public int getUsersCount(UserFilter filter) {
+        appendRoleGroup(filter);
+        return (Integer) sqlSession().selectOne(NS + ".selectUsersCount", filter);
+    }
+
+    private void appendRoleGroup(UserFilter filter) {
+        String role = filter.getRole();
+        for (Map.Entry<String, List<String>> group : GROUPS.entrySet()) {
+            if (group.getValue().contains(role)) {
+                filter.appendRole(group.getKey());
+            }
+        }
+    }
+
+    public List<String> getAllRoles() {
+        return ALL_ROLES;
     }
 }
