21f4148ecf2a3a01292f49c9085a243c2cbff6cd
heaven.iaa <heaven.iaa@931d702a-5a61-2881-6589-5da9c06b4b28>
2012-04-04 11:26:42 +0000

    change shared_to column

--- trunk/flexbuh-db/update/2012_04_04.sql
+++ trunk/flexbuh-db/update/2012_04_04.sql
@@ -3,13 +3,13 @@ DROP TABLE IF EXISTS `share`;
 CREATE TABLE `share` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
   `session_id` BIGINT(20) COMMENT 'Идентификатор сессии',
-  `shared_session_id` BIGINT(20) COMMENT 'Общий идентификатор сессии',
+  `shared_to_session_id` BIGINT(20) COMMENT 'Общий идентификатор сессии',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_share` (`session_id`, `shared_session_id`),
+  UNIQUE KEY `unique_share` (`session_id`, `shared_to_session_id`),
   KEY `key_session_id` (`session_id`),
-  KEY `key_shared_session_id` (`shared_session_id`),
+  KEY `key_shared_to_session_id` (`shared_to_session_id`),
   CONSTRAINT `fk_share__session` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`),
-  CONSTRAINT `fk_share_shared__session` FOREIGN KEY (`shared_session_id`) REFERENCES `session` (`id`)
+  CONSTRAINT `fk_share_shared__session` FOREIGN KEY (`shared_to_session_id`) REFERENCES `session` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Общие данные';
 
 INSERT INTO `update` (`date`, `date_version`) VALUES ('2012-04-04', 0);
