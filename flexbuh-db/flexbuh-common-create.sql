fe0b9a11eda01164c3dcde494a7129b46b868b95
heaven.iaa <heaven.iaa@931d702a-5a61-2881-6589-5da9c06b4b28>
2012-04-06 05:17:50 +0000

    add share and preference to create scripts

--- flexbuh-db/flexbuh-common-create.sql
+++ flexbuh-db/flexbuh-common-create.sql
@@ -539,5 +539,40 @@ CREATE TABLE `last_name` (
 )
 ENGINE = InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------
+-- Share
+-- --------------------------
+
+DROP TABLE IF EXISTS `share`;
+
+CREATE TABLE `share` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
+  `session_id` BIGINT(20) COMMENT 'Идентификатор сессии',
+  `shared_to_session_id` BIGINT(20) COMMENT 'Общий идентификатор сессии',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_share` (`session_id`, `shared_to_session_id`),
+  KEY `key_session_id` (`session_id`),
+  KEY `key_shared_to_session_id` (`shared_to_session_id`),
+  CONSTRAINT `fk_share__session` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`),
+  CONSTRAINT `fk_share_shared__session` FOREIGN KEY (`shared_to_session_id`) REFERENCES `session` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Общие данные';
+
+-- --------------------------
+-- Preference
+-- --------------------------
+
+DROP TABLE IF EXISTS `preference`;
+
+CREATE TABLE `preference` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор предпочтения',
+  `session_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор сессии',
+  `key` VARCHAR(255) NOT NULL COMMENT 'Ключ',
+  `value` VARCHAR(255) NOT NULL COMMENT 'Значение',
+  PRIMARY KEY (`id`),
+  KEY `key_session_id` (`session_id`),
+  UNIQUE KEY `unique_key` (`session_id`, `key`),
+  CONSTRAINT `fk_preference__session` FOREIGN KEY (`session_id`) REFERENCES `session` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Предпочтения пользователя';
+
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 
