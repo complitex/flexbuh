8ae4e72508def77be20ffd6ccc17ca97fcdb985d
heaven.iaa <heaven.iaa@931d702a-5a61-2881-6589-5da9c06b4b28>
2012-04-25 07:42:09 +0000

    fix person profile import

---
+++ trunk/flexbuh-document/src/main/java/org/complitex/flexbuh/document/service/ImportPersonProfileXMLService.java
@@ -0,0 +1,73 @@
+package org.complitex.flexbuh.document.service;
+
+import org.complitex.flexbuh.common.entity.PersonProfile;
+import org.complitex.flexbuh.common.entity.PersonType;
+import org.complitex.flexbuh.common.logging.EventCategory;
+import org.complitex.flexbuh.common.logging.EventModel;
+import org.complitex.flexbuh.common.logging.EventObjectFactory;
+import org.complitex.flexbuh.common.logging.EventObjectId;
+import org.complitex.flexbuh.common.service.ConfigBean;
+import org.complitex.flexbuh.common.service.ImportListener;
+import org.complitex.flexbuh.common.service.ImportXMLService;
+import org.complitex.flexbuh.common.service.PersonProfileBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.xml.bind.JAXBContext;
+import java.io.InputStream;
+
+/**
+ * @author Pavel Sknar
+ *         Date: 10.09.11 11:51
+ */
+@Stateless
+public class ImportPersonProfileXMLService extends ImportXMLService<PersonProfile> {
+    private final static Logger log = LoggerFactory.getLogger(ImportPersonProfileXMLService.class);
+
+    @EJB
+    private PersonProfileBean personProfileBean;
+
+    @EJB
+    private EventObjectFactory eventObjectFactory;
+
+    @EJB
+    private ConfigBean configBean;
+
+    public void process(Long sessionId, ImportListener<PersonProfile> listener, String name, InputStream inputStream) {
+        listener.begin();
+
+        try {
+            PersonProfile.RS rs = (PersonProfile.RS) JAXBContext.newInstance(PersonProfile.RS.class)
+                    .createUnmarshaller()
+                    .unmarshal(inputStream);
+
+            for (PersonProfile personProfile : rs.getRows()) {
+                personProfile.setSessionId(sessionId);
+
+                if (personProfile.getProfileName() == null) {
+                    personProfile.setProfileName(personProfile.getName());
+                }
+
+                if (PersonType.PHYSICAL_PERSON.equals(personProfile.getPersonType())){
+                    personProfile.parsePhysicalNames();
+                }
+
+                personProfileBean.save(personProfile);
+
+                listener.processed(personProfile);
+
+                log.info("Import person profile {}", new Object[]{personProfile, EventCategory.IMPORT,
+                        new EventObjectId(personProfile.getId()), new EventModel(PersonProfile.class.getName()),
+                        eventObjectFactory.getEventNewObject(personProfile)});
+            }
+
+            listener.completed();
+        } catch (Throwable th) {
+            log.warn("Cancel import user profile: " + name, th);
+
+            listener.error(th.getLocalizedMessage());
+        }
+    }
+}
