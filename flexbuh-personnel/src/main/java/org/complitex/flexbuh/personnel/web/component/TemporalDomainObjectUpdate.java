8aa650029e12f72bd7318eb357014d5861a1f300
pavel.sknar@gmail.com <pavel.sknar@gmail.com@931d702a-5a61-2881-6589-5da9c06b4b28>
2012-05-04 08:35:24 +0000

    Added field`s history panel

--- flexbuh-personnel/src/main/java/org/complitex/flexbuh/personnel/web/component/TemporalDomainObjectUpdate.java
+++ flexbuh-personnel/src/main/java/org/complitex/flexbuh/personnel/web/component/TemporalDomainObjectUpdate.java
@@ -1,7 +1,11 @@
 package org.complitex.flexbuh.personnel.web.component;
 
+import com.google.common.collect.Lists;
+import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.complitex.flexbuh.common.entity.TemporalDomainObject;
 import org.complitex.flexbuh.common.web.component.IAjaxUpdate;
+
+import java.util.List;
 
 /**
  * @author Pavel Sknar
@@ -11,11 +15,24 @@ public abstract class TemporalDomainObjectUpdate<T extends TemporalDomainObject>
 
     private T object;
 
+    private List<TemporalHistoryPanel<T>> panels = Lists.newArrayList();
+
     public T getObject() {
         return object;
     }
 
     public void setObject(T object) {
         this.object = object;
+    }
+
+    public void addToUpdate(TemporalHistoryPanel<T> panel) {
+        panels.add(panel);
+    }
+
+    @Override
+    public void onUpdate(AjaxRequestTarget target) {
+        for (TemporalHistoryPanel<T> panel : panels) {
+            panel.internalPrepareForUpdate(this);
+        }
     }
 }
