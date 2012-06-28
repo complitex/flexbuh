1c956ab3ec0d73b27adac1148695339f9fbe4d64
pavel.sknar@gmail.com <pavel.sknar@gmail.com@931d702a-5a61-2881-6589-5da9c06b4b28>
2012-06-28 09:17:38 +0000

    Change models of personnel

--- flexbuh-db/flexbuh-common-insert.sql
+++ flexbuh-db/flexbuh-common-insert.sql
@@ -2,7 +2,7 @@
 -- Update
 -- ------------------------------
 
-INSERT INTO `update` (`date`, `date_version`) VALUE ('2012-06-28', '0');
+INSERT INTO `update` (`date`, `date_version`) VALUE ('2012-06-28', '1');
 
 -- --------------------------------
 -- User
