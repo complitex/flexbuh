571dfaf0b1e43797d5e84b37a75cf92ca171827c
pavel.sknar@gmail.com <pavel.sknar@gmail.com@931d702a-5a61-2881-6589-5da9c06b4b28>
2012-06-28 08:24:47 +0000

    all users is 'AUTHORIZED' (except 'ANONYMOUS')

--- flexbuh-db/flexbuh-personel-create.sql
+++ flexbuh-db/flexbuh-personel-create.sql
@@ -68,21 +68,17 @@ ENGINE = InnoDB DEFAULT CHARSET=utf8;
 DROP TABLE IF EXISTS `position`;
 
 CREATE TABLE `position` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-  `session_id` BIGINT(20) NOT NULL,
-  `object_id` BIGINT(20),
-  `name` VARCHAR(255),
-  `rate_count` INTEGER,
-  `rate_type_id` BIGINT(20),
-  `min_salary` DECIMAL(15,2),
-  `max_salary` DECIMAL(15,2),
+  `id` BIGINT(20) NOT NULL,d
+  `version` BIGINT(20) NOT NULL,
+  `deleted` BOOLEAN,
+  `entry_into_force_date` DATETIME NOT NULL,
+  `completion_date` DATETIME,
+  `name` VARCHAR(255) NOT NULL,
+  `code` VARCHAR(255),
+  `payment_id` BIGINT
   `schedule_id` BIGINT(20),
-  `description` VARCHAR(255),
-  `current` BOOLEAN NOT NULL,
-  `date_start` DATE NOT NULL,
-  `updated` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `comment` VARCHAR(1000),
-  PRIMARY KEY (`id`),
+  `description` VARCHAR(1000),
+  PRIMARY KEY (`id`, `version`),
   KEY `key_position__schedule_id` (`schedule_id`),
   CONSTRAINT `fk_position__schedule` FOREIGN KEY (`schedule_id`) REFERENCES `schedule` (`id`)
 )
