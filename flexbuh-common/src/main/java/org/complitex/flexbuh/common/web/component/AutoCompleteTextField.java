1d9670d54430d5da202adfb25f9f15a7624eba13
pavel.sknar@gmail.com <pavel.sknar@gmail.com@931d702a-5a61-2881-6589-5da9c06b4b28>
2012-10-24 04:45:49 +0000

    Show all allowance types then pressed button down cursor

--- flexbuh-common/src/main/java/org/complitex/flexbuh/common/web/component/AutoCompleteTextField.java
+++ flexbuh-common/src/main/java/org/complitex/flexbuh/common/web/component/AutoCompleteTextField.java
@@ -1,9 +1,13 @@
 package org.complitex.flexbuh.common.web.component;
 
 import com.google.common.collect.Lists;
+import org.apache.wicket.extensions.ajax.markup.html.autocomplete.AutoCompleteSettings;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.util.string.Strings;
 import org.complitex.flexbuh.common.entity.DomainObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.Iterator;
@@ -17,27 +21,39 @@ import java.util.Locale;
 public abstract class AutoCompleteTextField<T extends DomainObject>
         extends org.apache.wicket.extensions.ajax.markup.html.autocomplete.AutoCompleteTextField<String> {
 
+    private static final Logger log = LoggerFactory.getLogger(AutoCompleteTextField.class);
 
     public AutoCompleteTextField(String id) {
         super(id);
+    }
+
+    protected AutoCompleteTextField(String id, AutoCompleteSettings settings) {
+        super(id, settings);
     }
 
     public AutoCompleteTextField(String id, PropertyModel<String> model) {
         super(id, model);
     }
 
+    protected AutoCompleteTextField(String id, IModel<String> object, AutoCompleteSettings settings) {
+        super(id, object, settings);
+    }
+
     @Override
     protected Iterator<String> getChoices(String input) {
+        /*
         if (Strings.isEmpty(input)) {
             List<String> emptyList = Collections.emptyList();
             return emptyList.iterator();
-        }
+        }*/
 
         List<String> choices = Lists.newArrayListWithCapacity(getCapacity());
 
         for (T organizationType : getDomainObjects(input, getLocale())) {
             choices.add(getDomainObjectName(organizationType, getLocale()));
         }
+
+        log.debug("Choices: {}", choices);
 
         return choices.iterator();
     }
